0640d1103a5b6dcb040d79cb69d9cd4ac2531e79
Cory Fitz <coryalanfitz@gmail.com>
2025-03-12 23:29:37 -0400

    first attempt at adding tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from "react-native";
 
 // Required for CSS to work on Expo Web.
 import './stylex.css';
+import './index.css'
 // Required for Fast Refresh to work on Expo Web
 import '@expo/metro-runtime';
 
@@ -10,11 +11,7 @@ import { html } from 'react-strict-dom';
 export default function Index() {
   return (
     <html.div
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 flex justify-center items-center"
     >
       <html.p>Edit app/index.tsx to edit this screen.</html.p>
     </html.div>
