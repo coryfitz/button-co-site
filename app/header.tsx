4cb75d3164aa63e42110dce828d81f60340305c1
Cory Fitz <coryalanfitz@gmail.com>
2025-03-17 22:20:18 -0400

    factored out header, added home button

---
+++ app/header.tsx
@@ -0,0 +1,27 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import "./global.css";
+
+export default function Header() {
+  return (
+    <View className="flex-1 bg-white">
+      {/* Header */}
+      <View className="w-full py-4 px-6 bg-blue-600 flex-row justify-between items-center">
+        {/* Title on the left */}
+        <Link href="/">
+            <Text className="text-white text-2xl font-bold">The Button Company</Text>
+        </Link>
+
+        {/* Navigation Links on the right */}
+        <View className="flex-row space-x-4">
+          <Link href="/about">
+            <Text className="text-white text-lg">About</Text>
+          </Link>
+          <Link href="/tutorials">
+            <Text className="text-white text-lg">Tutorials</Text>
+          </Link>
+        </View>
+      </View>
+      </View>
+  );
+}
--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -1,25 +1,11 @@
 import { Text, View} from "react-native";
-import { Link } from "expo-router";
 import "./global.css";
+import Header from "./header"
 
-export default function Index() {
+export default function Tutorials() {
   return (
-    <View className="flex-1 bg-white">
-      {/* Header */}
-      <View className="w-full py-4 px-6 bg-blue-600 flex-row justify-between items-center">
-        {/* Title on the left */}
-        <Text className="text-white text-2xl font-bold">The Button Company</Text>
-
-        {/* Navigation Links on the right */}
-        <View className="flex-row space-x-4">
-          <Link href="/about">
-            <Text className="text-white text-lg">About</Text>
-          </Link>
-          <Link href="/tutorials">
-            <Text className="text-white text-lg">Tutorials</Text>
-          </Link>
-        </View>
-      </View>
+    <View>
+      <Header />
 
       {/* Main Content */}
       <View className="flex-1 justify-center items-center">
