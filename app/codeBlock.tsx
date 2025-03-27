64ea60276100554fb3bcadc306125e9ba197eaa7
Cory Fitz <coryalanfitz@gmail.com>
2025-03-26 23:36:03 -0400

    added button effect

--- app/codeBlock.tsx
+++ app/codeBlock.tsx
@@ -8,19 +8,23 @@ import { atomOneDark } from 'react-syntax-highlighter/styles/hljs';
 
 //rn syntax highlighter doesn't work with the rounded corners on RN web
 
-export default function CodeBlock({ code }: { code: string }) {
+//the button doesn't do anything visually when you click on it
 
+export default function CodeBlock({ code }: { code: string }) {
   return (
     <View className="m-2">
       <SyntaxHighlighter language="javascript" style={atomOneDark} className="rounded-xl bg-black p-4">
         {code}
       </SyntaxHighlighter>
-      <Pressable
-  className="bg-blue-600 px-4 py-2 rounded-xl mt-2" onPress={() => Clipboard.setString(code)} >
-    <Text className="text-white text-center text-base font-medium">
+      <Pressable 
+  className="bg-blue-600 px-4 py-2 rounded-xl mt-2 pressable:bg-blue-700 active:opacity-80"
+  onPress={() => Clipboard.setString(code)}
+>
+  <Text className="text-white text-center text-base font-medium">
     Copy
-    </Text>
-    </Pressable>
+  </Text>
+</Pressable>
+
     </View>
   );
-};
+}
