3cf69505d4176766c6f1d1411fb5c14ad7f76f06
Cory Fitz <coryalanfitz@gmail.com>
2025-03-27 23:49:49 -0400

    added max width and centered horizontally

--- app/codeBlock.tsx
+++ app/codeBlock.tsx
@@ -5,26 +5,27 @@ import Clipboard from '@react-native-clipboard/clipboard';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/styles/hljs';
 
-
-//rn syntax highlighter doesn't work with the rounded corners on RN web
-
-//the button doesn't do anything visually when you click on it
+// rn syntax highlighter doesn't work with the rounded corners on RN web
+// the button doesn't do anything visually when you click on it
 
 export default function CodeBlock({ code }: { code: string }) {
   return (
-    <View className="m-2">
-      <SyntaxHighlighter language="javascript" style={atomOneDark} className="rounded-xl bg-black p-4">
+    <View className="m-2 max-w-2xl w-full mx-auto">
+      <SyntaxHighlighter
+        language="javascript"
+        style={atomOneDark}
+        className="rounded-xl bg-black p-4"
+      >
         {code}
       </SyntaxHighlighter>
       <Pressable 
-  className="bg-blue-600 px-4 py-2 rounded-xl mt-2 pressable:bg-blue-700 active:opacity-80"
-  onPress={() => Clipboard.setString(code)}
->
-  <Text className="text-white text-center text-base font-medium">
-    Copy
-  </Text>
-</Pressable>
-
+        className="bg-blue-600 px-4 py-2 rounded-xl mt-2 pressable:bg-blue-700 active:opacity-80"
+        onPress={() => Clipboard.setString(code)}
+      >
+        <Text className="text-white text-center text-base font-medium">
+          Copy
+        </Text>
+      </Pressable>
     </View>
   );
 }
