865295c0a114f208c27806bbb8ecdb6c2555023a
Cory Fitz <coryalanfitz@gmail.com>
2025-04-04 23:35:39 -0400

    added some consistency for all tutorials

--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -111,11 +111,19 @@ export default function Tutorials() {
           </Text>
           <Tabs currentTab={tab3} setCurrentTab={setTab3} />
           {tab3 === "python" ? (
+            <>
             <Text className="text-black text-base leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Text>
-          ) : (
-            <Text className="text-black italic">Rust version coming soon...</Text>
+            <CodeBlock code={pythonCode} language="python" />
+          </>
+        ) : (
+          <>
+            <Text className="text-black text-base leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
+            <CodeBlock code={rustCode} language="rust" />
+          </>
           )}
         </View>
 
