c98504d0844980787f3fbe8b53c4ee92bb54bdf4
Cory Fitz <coryalanfitz@gmail.com>
2025-04-18 23:14:07 -0400

    more info to tutorial

--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -84,7 +84,7 @@ export default function Tutorials() {
                 1. Set up ESPHome on your Home Assistant
                 <br></br>
                 <br></br>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Click the button on this page: https://esphome.io/guides/getting_started_hassio
               </Text>
               <CodeBlock code={
                 `binary_sensor:
