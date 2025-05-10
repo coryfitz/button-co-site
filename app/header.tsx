3371ea59ae106e304a2a2b32480ccc8cee1d854e
Cory Fitz <coryalanfitz@gmail.com>
2025-05-10 00:37:22 -0400

    updated logo

--- app/header.tsx
+++ app/header.tsx
@@ -1,13 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Link } from "expo-router";
 import "./global.css";
+
+import buttonLogo from '../assets/images/logo-white.png';
+
 
 export default function Header() {
   return (
 
       <View className="w-full py-4 px-6 flex-row justify-between items-center">
         {/* Title on the left */}
-        <Link href="/">
+        <Link href="/" className="flex-row items-center space-x-2">
+        <Image
+          source={buttonLogo}
+          style={{ width: 40, height: 40 }}
+          className="relative top-3"
+        />
             <Text className="text-white text-2xl font-bold">The Button Company</Text>
         </Link>
 
