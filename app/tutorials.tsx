635e07bcd353912dd028681ee8924a0631d8e0e9
Cory Fitz <coryalanfitz@gmail.com>
2025-05-11 23:35:17 -0400

    update tutporial

--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -133,6 +133,7 @@ export default function Tutorials() {
                 <br></br>
                 <br></br>
                 <br></br>
+                <br></br>
               </Text>
 
               <CodeBlock code={
