63abeb643092ee6dff298319be9dd0582f605930
Cory Fitz <coryalanfitz@gmail.com>
2025-06-06 23:51:07 -0400

    update

--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -232,7 +232,7 @@ Check that the binary sensor toggles from off to on when the button is pressed
           tab2 === "MicroPython" ? (
             <>
               <Text className="text-black text-base leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Enter the following code
               </Text>
               <CodeBlock code={`
 import network
