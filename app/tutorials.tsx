46d2c6e2fda1c9e57a5bdbe459bdbd8425aa98ee
Cory Fitz <coryalanfitz@gmail.com>
2025-06-08 23:12:11 -0400

    update

--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -305,7 +305,7 @@ while True:
         ) : (
           <>
             <Text className="text-black text-base leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Error
             </Text>
             <CodeBlock code={rustCode} language="rust" />
           </>
