a4fc57c0f38f1c6477316c7221a931bd35d911e9
Cory Fitz <coryalanfitz@gmail.com>
2025-06-25 00:28:10 -0400

    update

--- app/tutorials.tsx
+++ app/tutorials.tsx
@@ -386,6 +386,8 @@ if __name__ == "__main__":
         </View>
         
 
+
+
         {/* Card 3 */}
         <View className="bg-white rounded-2xl p-4 mb-6 shadow-md">
           <Text className="text-center text-2xl font-bold text-black mb-2">
